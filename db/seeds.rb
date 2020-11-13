171e8b73c2eaeaeb47e6f6bc0e7c2836d278e9fb
Steven Le Moal <steven.lemoal@gmail.com>
2020-11-13 05:24:27 +0100

    Add dotenv - Protect my secret data in .env file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,12 @@
 # require 'open-uri'
 # require 'json'
 
-# url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-# ingredients = JSON.parse(open(url).read)
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients = JSON.parse(open(url).read)
 
-# ingredients["drinks"].each do |ingredient|
-#   Ingredient.create!(name: ingredient["strIngredient1"])
-# end
+ingredients["drinks"].each do |ingredient|
+  Ingredient.create!(name: ingredient["strIngredient1"])
+end
 
 cocktails = Cocktail.create!([{ name: 'B & B'}, { name: 'The Blenheim'}, { name: 'Blow my Skull Off'}, { name: 'Brandy Alexander'}, { name: 'Brandy Manhattan'}, { name: 'Brandy Sour (Cyprus)'},
-{ name: 'Brandy Sour/Brandy Daisy'}, { name: 'Chicago Cocktail'}, { name: 'Curacao Punch'}, { name: 'Diki-Diki'}, { name: 'Four Score'}, { name: 'French Connection'}, { name: 'Hennchata'}, { name: "Horse's Neck"},
-{ name: 'Incredible Hulk'}, { name: 'Jack Rose'}, { name: 'Panama'}, { name: 'Paradise'}, { name: 'Pisco Sour'}, { name: 'Porto flip'}, { name: 'Savoy Affair'}, { name: 'Savoy Corpse Reviver'},
-{ name: 'Sazerac'}, { name: 'Sidecar'}, { name: 'Singapore Sling'}, { name: 'Stinger'}, { name: 'Tom and Jerry'}])
+{ name: 'Brandy Sour/Brandy Daisy'}, { name: 'Chicago Cocktail'}, { name: 'Curacao Punch'}, { name: 'Diki-Diki'}, { name: 'Four Score'}, { name: 'French Connection'}, { name: 'Hennchata'}, { name: "Horse's Neck"}])
